93e3caac090b77f76153912cfa7cd63aed8c915f
Pavel Bobryshev <bobr.8590@gmail.com>
2020-08-07 16:19:16 +0300

    child and parents

---
+++ components/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
+import faker from 'faker';
+
+const App = () =>{
+    return (
+        <div className="ui container comments">
+            <ApprovalCard > 
+                <CommentDetail author="Sam" timeAgo={faker.date.weekday()} comment="Whoa" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard > 
+                <CommentDetail author="Alex" timeAgo={faker.date.weekday()} comment="Nice" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard > 
+                <CommentDetail author="Jane" timeAgo={faker.date.weekday()} comment="Ah" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
