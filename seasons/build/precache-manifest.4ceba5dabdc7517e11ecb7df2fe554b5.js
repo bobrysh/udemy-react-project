5f533aab4509b24a01c3c0b49d5c2de84fea1802
Pavel Bobryshev <bobr.8590@gmail.com>
2020-08-10 11:15:57 +0300

    try deploy

---
+++ seasons/build/precache-manifest.4ceba5dabdc7517e11ecb7df2fe554b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ef9fcc8eb4d1a14a6043233f5e6270",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2dd7be7363b09430420",
+    "url": "/static/css/main.972d08d5.chunk.css"
+  },
+  {
+    "revision": "fa354485be05596676f5",
+    "url": "/static/js/2.af9bdbca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.af9bdbca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2dd7be7363b09430420",
+    "url": "/static/js/main.6d7309a2.chunk.js"
+  },
+  {
+    "revision": "dcb882432a296688e2bc",
+    "url": "/static/js/runtime-main.99ab51a2.js"
+  }
+]);
