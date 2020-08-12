b8238c3bfaf2c80962f15888e2ae908999d22471
Pavel Bobryshev <bobr.8590@gmail.com>
2020-08-12 17:23:50 +0300

    widgets progress, navigation will added soon

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,29 +1,87 @@
-import React from 'react';
-// import Accordion from './components/Accordion';
+import React, {useState} from 'react';
+import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
-// const items = [
-//     {
-//         title:'What is React?',
-//         content: 'React is a front end javascript framework'
-//     },
-//     {
-//         title:'Why use React?',
-//         content: 'React is a favorite JS library among engineers'
-//     },
-//     {
-//         title:'How do you use React?',
-//         content: 'You use React by creating components'
-//     },
-// ]
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
+  },
+];
 
-const App = () => {
-    return (
-        <div>
-            {/* <Accordion items={items} /> */}
-            <Search />
-        </div>
-    )
+// const options = [
+//   {
+//     label: 'The Color Red',
+//     value: 'red',
+//   },
+//   {
+//     label: 'The Color Green',
+//     value: 'green',
+//   },
+//   {
+//     label: 'A Shade of Blue',
+//     value: 'blue',
+//   },
+// ];
+
+const showAccordion = () => {
+  if(window.location.pathname === '/'){
+    return <Accordion items={items}/>
+  }
 }
 
-export default App;
+const showList = () => {
+  if(window.location.pathname === '/list'){
+    return <Search />
+  }
+}
+
+const showDropdown = () => {
+  if(window.location.pathname === '/dropdown'){
+    return <Dropdown />
+  }
+}
+
+const showTranslate = () => {
+  if(window.location.pathname === '/translate'){
+    return <Translate />
+  }
+}
+
+
+export default () => {
+  // const [selected, setSelected] = useState(options[0]);
+  // const [showDropdown, setShowDropdown] = useState(true);
+
+  return (
+    <div>
+      {/* <button onClick={()=>{
+        setShowDropdown(!showDropdown)
+      }}>Toggle Dropdown</button>
+      {showDropdown ?
+      <Dropdown 
+       selected={selected}
+       onSelectedChange={setSelected}
+       options={options} 
+       /> : null
+       } */}
+      {/* <Search /> */}
+      {showAccordion()}
+      {showList()}
+      {showDropdown()}
+      {showTranslate()}
+
+      {/* {<Translate />} */}
+    </div>
+  );
+};
--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,93 +1,72 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('Programming')
-    const [results, setResults] = useState([])
+  const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  const [results, setResults] = useState([]);
 
-    useEffect(()=>{
-        // #1 Способ запустить асинк в useEffect
-        const search = async () => {
-            const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
-                params:{
-                    action:'query',
-                    list:'search',
-                    origin:'*',
-                    format:'json',
-                    srsearch: term,
-                },
-            });
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
 
-            setResults(data.query.search);
-        }
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
-        if(term && !results.length){
-            search();
-        } else{
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: debouncedTerm,
+        },
+      });
 
-            const timeoutId = setTimeout(()=>{
-                if(term){
-                    search();
-                }
-            },800)
+      setResults(data.query.search);
+    };
+    search();
+  }, [debouncedTerm]);
 
-            return () => {
-                clearTimeout(timeoutId)
-            }
-        }
-
-
-
-
-
-
-        // #2 Способ запустить асинк в useEffect
-        // (async () => {
-        //     await axios.get()
-        // })();
-
-        // #3 Способ запустить асинк в useEffect
-        // axios.get('asdasd')
-        // .then((respone) => {
-        //     console.log(respone.data);
-        // });
-    },[term])
-
-    const renderedResults = results.map((result)=>{
-        return (
-            <div className='item' key={result.pageid}>
-                <div className="right floated content">
-                </div>
-                <div className='content'>
-                    <div className='header'>
-                        <h4>{result.title}</h4>
-                    </div>
-                    <span dangerouslySetInnerHTML={{__html:result.snippet}}></span>
-                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
-                </div>
-            </div>
-        )
-    });
-
+  const renderedResults = results.map((result) => {
     return (
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <label>Enter Search Term</label>
-                    <input 
-                        className="input"
-                        value={term}
-                        onChange={e=>setTerm(e.target.value)}
-                    />
-                </div>
-            </div>
-            <div className='ui celled list'>
-                <div>
-                    {renderedResults}
-                </div>
-            </div>
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
         </div>
-    )
-}
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
+};
 
 export default Search;
